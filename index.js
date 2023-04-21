59c5ce71675707eb94287006cea22ab451207912
emonhasan007 <emon45088@gmail.com>
2023-04-21 12:44:43 +0600

    all news data server site added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000 ;
 
-const category = require('./data/category.json')
+const category = require('./data/category.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 app.get('/' , (req , res)=>{
@@ -12,7 +13,28 @@ app.get('/' , (req , res)=>{
 
 app.get('/category' , (req , res )=>{
     res.send(category);
-})
+});
+
+app.get('/news' , (req , res) =>{
+    res.send(news);
+});
+
+app.get('/news/:id' , (req , res)=>{
+    const id = req.params.id;
+    const selectId = news.find(n=>n._id === id);
+    res.send(selectId);
+});
+
+app.get('/category/:id' , (req , res)=>{
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(news);
+    }else{
+        const categoryNews = news.filter( n => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+    }
+    
+} )
 
 app.listen( port , ()=>{
     console.log(`Dragon API is running on port ${port}`);
